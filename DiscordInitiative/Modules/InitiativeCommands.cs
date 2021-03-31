0e93812b8d7ff982a5d70edbf9df4b077849ac49
Ben Sullivan <git@sulli.ca>
2021-03-30 22:43:51 -0700

    First draft
    It lives!

---
+++ DiscordInitiative/Modules/InitiativeCommands.cs
@@ -0,0 +1,155 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using static DiscordInitiative.Program;
+
+namespace DiscordInitiative.Modules
+{
+    public class InitiativeCommands : ModuleBase
+    {
+        [Command("init")]
+        public async Task InitCommand()
+        {
+            var sb = new StringBuilder();
+            
+            string actorType = "PC";
+            string actorName = Context.User.Username;
+
+            Order.Add(actorName, actorType);
+
+            // build out the reply
+            foreach (var line in Order.GetOrderList())
+            {
+                sb.AppendLine(line);
+            }
+            
+            // send simple string reply
+            await ReplyAsync(sb.ToString());
+        }
+
+        [Command("init")]
+        public async Task InitCommandWithArgs([Remainder] string args)
+        {
+            var sb = new StringBuilder();
+
+            string actorType = "PC";
+            var argList = args.Split(" ");
+            string actorName = Context.User.Username;
+            if (argList.Length == 1)
+            {
+                actorName = argList[0];
+            }
+            if (argList.Length == 2)
+            {
+                actorName = argList[0];
+                actorType = argList[1];
+            }
+
+            if (argList.Length > 2)
+            {
+                actorName = "";
+                for (int i = 0;  i < argList.Length - 1; i++)
+                {
+                    actorName += argList[i] + " ";
+                }
+
+                actorType = argList[^1];
+            }
+            Order.Add(actorName,actorType);
+
+            // build out the reply
+            foreach (var line in Order.GetOrderList())
+            {
+                sb.AppendLine(line);
+            }
+
+            // send simple string reply
+            await ReplyAsync(sb.ToString());
+        }
+
+    //    [Command("8ball")]
+    //    [Alias("ask")]
+    //    [RequireUserPermission(GuildPermission.KickMembers)]
+    //    public async Task AskEightBall([Remainder] string args = null)
+    //    {
+    //        // I like using StringBuilder to build out the reply
+    //        var sb = new StringBuilder();
+    //        // let's use an embed for this one!
+    //        var embed = new EmbedBuilder();
+
+    //        // now to create a list of possible replies
+    //        var replies = new List<string>();
+
+    //        // add our possible replies
+    //        replies.Add("yes");
+    //        replies.Add("no");
+    //        replies.Add("maybe");
+    //        replies.Add("hazzzzy....");
+
+    //        // time to add some options to the embed (like color and title)
+    //        embed.WithColor(new Color(0, 255, 0));
+    //        embed.Title = "Welcome to the 8-ball!";
+
+    //        // we can get lots of information from the Context that is passed into the commands
+    //        // here I'm setting up the preface with the user's name and a comma
+    //        sb.AppendLine($",");
+    //        sb.AppendLine();
+
+    //        // let's make sure the supplied question isn't null 
+    //        if (args == null)
+    //        {
+    //            // if no question is asked (args are null), reply with the below text
+    //            sb.AppendLine("Sorry, can't answer a question you didn't ask!");
+    //        }
+    //        else
+    //        {
+    //            // if we have a question, let's give an answer!
+    //            // get a random number to index our list with (arrays start at zero so we subtract 1 from the count)
+    //            var answer = replies[new Random().Next(replies.Count - 1)];
+
+    //            // build out our reply with the handy StringBuilder
+    //            sb.AppendLine($"You asked: [****]...");
+    //            sb.AppendLine();
+    //            sb.AppendLine($"...your answer is [****]");
+
+    //            // bonus - let's switch out the reply and change the color based on it
+    //            switch (answer)
+    //            {
+    //                case "yes":
+    //                    {
+    //                        embed.WithColor(new Color(0, 255, 0));
+    //                        break;
+    //                    }
+    //                case "no":
+    //                    {
+    //                        embed.WithColor(new Color(255, 0, 0));
+    //                        break;
+    //                    }
+    //                case "maybe":
+    //                    {
+    //                        embed.WithColor(new Color(255, 255, 0));
+    //                        break;
+    //                    }
+    //                case "hazzzzy....":
+    //                    {
+    //                        embed.WithColor(new Color(255, 0, 255));
+    //                        break;
+    //                    }
+    //            }
+    //        }
+
+    //        // now we can assign the description of the embed to the contents of the StringBuilder we created
+    //        embed.Description = sb.ToString();
+
+    //        // this will reply with the embed
+    //        await ReplyAsync(null, false, embed.Build());
+    //    }
+    }
+}
--- DiscordInitiative/Program.cs
+++ DiscordInitiative/Program.cs
@@ -2,31 +2,183 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using DiscordInitiative;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using DiscordInitiative.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualBasic;
 
 namespace DiscordInitiative
 {
+    public class Deck
+    {
+        internal Random r = new Random();
+        internal string Variant;
+        internal List<Card> Cards = new List<Card>();
+
+        public bool DeckExists { get; }
+
+        public Deck(string deckVariant)
+        {
+            JObject o1 = JObject.Parse(File.ReadAllText("decks.json"));
+            
+
+            Variant = deckVariant;
+                foreach (var deckObj in o1["decks"].Children())
+                {
+                    if (deckObj["deckName"]?.ToString() == Variant)
+                    {
+                        DeckExists = true;
+                        foreach (var cardsObj in deckObj["cards"].Children())
+                        {
+                            Cards.Add(new Card((int)cardsObj["value"], cardsObj["name"]?.ToString(), cardsObj["suit"]?.ToString()));
+                        }
+                    }
+                }
+
+                if (!DeckExists)
+                {
+                    Console.WriteLine("Couldn't find the requested deck " + Variant + ". Sorry :(");
+                }
+        }
+
+        public Card Draw()
+        {
+            int yourCard;
+            while (true)
+            {
+                yourCard = r.Next(0, 53);
+                if (!Cards[yourCard].Drawn)
+                    break;
+            }
+
+            Cards[yourCard].Drawn = true;
+            return Cards[yourCard];
+
+        }
+
+        public int RemainingCardCount()
+        {
+            int count = 0;
+            foreach (var card in Cards)
+            {
+                if (!card.Drawn)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public void Shuffle()
+        {
+
+        }
+
+    }
+    public class Card
+    {
+        public Card(int value, string cardName, string suit)
+        {
+            this.Value = value;
+            this.CardName = cardName;
+            this.Suit = suit;
+            this.SuitEmoji = this.Suit switch
+            {
+                "spades" => ":spades:",
+                "hearts" => ":hearts:",
+                "diamonds" => ":diamonds:",
+                "clubs" => ":clubs:",
+                _ => ":black_joker:"
+            };
+        }
+
+
+        public string SuitEmoji { get; }
+        public string Suit { get; }
+        public string CardName { get; }
+        public int Value { get; }
+
+        public bool Drawn { get; set; }
+    }
+
+    public class Order
+    {
+        private static Dictionary<string, int> orderDictionary = new Dictionary<string, int>();
+        public static void Add(string actorName,string actorType)
+        {
+            Card newCard = Program.Deck.Draw();
+            orderDictionary.Add(Program.GlobalConfigStruct._lineFormat
+                .Replace("[SUIT]", newCard.SuitEmoji)
+                .Replace("[CARD]", newCard.CardName)
+                .Replace("[CharacterName]", actorName)
+                .Replace("[AlliedStatus]", actorType),newCard.Value);
+
+        }
+
+        public static List<String> GetOrderList()
+        {
+            List<String> orderList = new List<string>();
+            var orderListDictionary = orderDictionary.ToList();
+            orderListDictionary.Sort((pair, valuePair) => pair.Value.CompareTo(valuePair.Value));
+            foreach (var row in orderListDictionary)
+            {
+                orderList.Insert(0,row.Key.ToString() + " (" + row.Value + ")");
+            }
+
+            return orderList;
+        }
+
+    }
+
     public class Program
     {
+        public static Deck Deck;
+        public static Order Order;
+
+        /*
+		 *
+		 * Add your character to the roll (default to your name)
+		 * Name your character (allowing enemies)
+		 * Draw your cardName for the initiative
+		 * Command to end the turn order and recalculate with existing options
+		 * Command to edit your name
+		 * Command to override your cardName/roll
+		 * Handle HOLD cases
+		 * Handle surprise cases
+		 * Allow identifying enemies
+		 *
+		 */
+
+        private DiscordSocketClient _client;
         public struct GlobalConfigStruct
         {
             public static string _botToken = "";
-		}
+            public static string _variantDeck = "";
+            public static string _lineFormat = "> [SUIT] [CARD] - **[CharacterName]** - _[AlliedStatus]_";
+        }
 
         public static void SetArgs(string[] args)
         {
             if (args.Length > 0)
             {
-
                 foreach (string arg in args)
                 {
-                    Console.WriteLine("Evaluating: " + arg);
                     if (arg.Contains("--token="))
                     {
                         GlobalConfigStruct._botToken = arg.Split("=")[1];
                     }
-
+                    if (arg.Contains("--deck="))
+                    {
+                        GlobalConfigStruct._variantDeck = arg.Split("=")[1];
+                    }
                 }
             }
         }
@@ -37,11 +189,22 @@ namespace DiscordInitiative
 		public async Task MainAsync(string[] args)
 		{
 			SetArgs(args);
-			DiscordSocketClient client = new DiscordSocketClient();
-			client.Log += Log;
-			client.LoginAsync(TokenType.Bot, GlobalConfigStruct._botToken);
-			client.StartAsync();
-			await Task.Delay(-1);
+            Deck = new Deck(GlobalConfigStruct._variantDeck);
+            Order = new Order();
+
+            using (var services = ConfigureServices())
+
+                if (Deck.DeckExists)
+                {
+                    var client = services.GetRequiredService<DiscordSocketClient>();
+                _client = client;
+                    client.Log += Log;
+                client.LoginAsync(TokenType.Bot, GlobalConfigStruct._botToken);
+                client.StartAsync();
+                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+                await Task.Delay(-1);
+                }
+            Console.WriteLine("Mischief managed. Closing.");
 		}
 
 		private Task Log(LogMessage msg)
@@ -50,7 +213,23 @@ namespace DiscordInitiative
 			return Task.CompletedTask;
 		}
 
-	}
+        private ServiceProvider ConfigureServices()
+        {
+            // this returns a ServiceProvider that is used later to call for those services
+            // we can add types we have access to here, hence adding the new using statement:
+            // using csharpi.Services;
+            // the config we build is also added, which comes in handy for setting the command prefix!
+            return new ServiceCollection()
+                //.AddSingleton(_config)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+        }
+
+    }
+
+
 
 	public class LoggingService
     {
@@ -73,6 +252,4 @@ namespace DiscordInitiative
 			return Task.CompletedTask;
 		}
 	}
-
-
 }
